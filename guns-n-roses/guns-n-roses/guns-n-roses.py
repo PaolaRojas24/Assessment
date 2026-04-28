3176ca9f73239526b0cc7b33e89249fbb96c3949
Paola <paolarojasdominguez24@gmail.com>
2026-04-28 12:18:02 -0600

    Empty Package

---
+++ guns-n-roses/guns-n-roses/guns-n-roses.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from guns-n-roses.')
+
+
+if __name__ == '__main__':
+    main()
--- guns-n-roses/setup.py
+++ guns-n-roses/setup.py
@@ -1,6 +1,4 @@
 from setuptools import find_packages, setup
-import os
-from glob import glob
 
 package_name = 'guns-n-roses'
 
@@ -12,21 +10,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
-        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
-        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.stl'))),
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Paola Rojas',
     maintainer_email='a01737136@tec.mx',
-    description='Puzzlebot Kinematic Sim',
+    description='TF2 Examples',
     license='Apache-2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
+            'guns-n-roses = guns-n-roses.guns-n-roses:main'
         ],
     },
 )
